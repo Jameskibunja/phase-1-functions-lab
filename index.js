62707a499e868aa25f2672c3a542708060eae2d3
JAMES <jameskibunja17@gmail.com>
2022-12-07 02:11:50 +0300

    Competed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+ const distanceFromHqInBlocks = (block) =>{
+     if (block<42){
+        return(42-block);
+     }
+     else{
+        return(block-42);
+     }
+ }
+const distanceFromHqInFeet = (block) => {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+const distanceTravelledInFeet = (s,d) =>{
+    if (d>s){
+        return((d-s)*264);
+    }
+    else {
+        return((s-d)*264);
+    }
+}
+const calculatesFarePrice = (s,d) => {
+    let distance = distanceTravelledInFeet(s,d);
+
+    if (distance <= 400){
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else if (distance > 2500){
+        return 'cannot travel that far';
+    }
+}
